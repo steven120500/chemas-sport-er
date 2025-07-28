18325c806205f561b7b276bf66272f583f018757
Steven Corrales <scorrales18@gmail.com>
2025-07-28 15:55:40 -0600

    Version 1.0.5

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://chemas-backend.onrender.com/api',
+  baseURL: 'https://chemas-backend.onrender/api',
 });
 
 export default api;
