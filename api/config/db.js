de7c446b75fe5ab4a910263e8437f40c933d547b
Steven Corrales <scorrales18@gmail.com>
2025-07-29 19:20:33 -0600

    Version 1.3.2

--- api/config/db.js
+++ api/config/db.js
@@ -2,12 +2,13 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('✅ MongoDB Atlas conectado');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB conectado: ${conn.connection.host}`);
   } catch (error) {
-    console.error('❌ Error al conectar con MongoDB Atlas:', error);
+    console.error(`Error de conexión: ${error.message}`);
     process.exit(1);
   }
 };
 
 export default connectDB;
+
