31c9efab5cc0b87c9a8c2c78a2c96bd42cbde1df
Steven Corrales <scorrales18@gmail.com>
2025-08-04 15:22:30 -0600

    Version 1.3.8

--- api/models/User.js
+++ api/models/User.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true, select: false },
   roles: [{ type: String }], // Ej: ['admin', 'agregar_producto', 'ver_pedidos']
   isSuperUser: { type: Boolean, default: false } // Solo uno debe tener true
 });
