6280133ee2d5582f68857d09d7630aceb5ce3fb1
Steven Corrales <scorrales18@gmail.com>
2025-08-20 19:18:20 -0600

    Version 2.0.7

--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -168,7 +168,7 @@ router.post("/", upload.single("image"), async (req, res) => {
           name: req.body.name,
           price: req.body.price,
           type: req.body.type,
-          sizes: req.body.sizes,
+          sizes: JSON.parse(req.body.sizes),
           imageSrc: result.secure_url, // 🔹 se guarda la URL de Cloudinary
         });
 
