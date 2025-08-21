53302fb5986673e319c86d0c83a96a62b94b1182
Steven Corrales <scorrales18@gmail.com>
2025-08-20 19:03:39 -0600

    Version 2.0.6

--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -4,6 +4,7 @@ import Product from '../models/Product.js';
 import History from '../models/History.js';
 import attachUser from '../middleware/attachUser.js'; 
 import cloudinary from '../config/cloudinary.js'; // o donde tengas la config
+import multer from 'multer';
 
 const router = express.Router();
 
