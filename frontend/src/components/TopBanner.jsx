154bdc80094bbf7de74b66390197dbaea85d8b22
Steven Corrales <scorrales18@gmail.com>
2025-08-26 18:14:38 -0600

    Version 2.9.2

--- frontend/src/components/TopBanner.jsx
+++ frontend/src/components/TopBanner.jsx
@@ -4,7 +4,7 @@ function TopBanner() {
   const messages = [
     "STOCK DE +2000 CHEMAS",
     "REGALAMOS CHEMAS LOS VIERNES EN NUESTRO CHEMAFEST",
-    "METODOS DE PAGO SINPE, TRANFERENCIA Y TARJETA",
+    "METODOS DE PAGO SINPE, TRANSFERENCIA, EFECTIVO Y PAGO CON TARJETA",
     "SOMOS CHEMASPORT ER",
   ];
   const [currentIndex, setCurrentIndex] = useState(0);
